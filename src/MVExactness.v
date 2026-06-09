2b1e3c06d6ace59cec33e594de39dd2287aaf145
choco-bear <kdy030529@gmail.com>
2026-06-09 22:37:38 +0900

    stated the exactness of MV sequence

--- src/MVExactness.v
+++ src/MVExactness.v
@@ -1,5 +1,24 @@
-From HoTT Require Import HoTT.
+From HoTT Require Import HoTT Utf8.
 
 Require Import Basics.
 Require Import Cohomology.
 Require Import MVSequence.
+
+Set Implicit Arguments.
+
+Section MVExactness.
+  Context {_Funext: Funext} {_Univalence: Univalence}.
+  Context {C A B: Type} (f: C → A) (g: C → B).
+  Context [G: AbGroup] (n: nat).
+
+  Definition IsExact {F X Y: pType} := @ExactSequence.IsExact (Tr (-1)) F X Y.
+
+  Theorem mv_exact_res_diff: IsExact (mv_res' f g G n) (mv_diff' f g G n).
+  Proof. (* TODO *) Admitted.
+
+  Theorem mv_exact_diff_delta: IsExact (mv_diff' f g G n) (mv_delta' f g G n).
+  Proof. (* TODO *) Admitted.
+
+  Theorem mv_exact_delta_res: IsExact (mv_delta' f g G n) (mv_res' f g G (S n)).
+  Proof. (* TODO *) Admitted.
+End MVExactness.
